b441823997789b53ffb06ed0c982c8182c0e9951
malongbo <751824643@qq.com>
2019-07-25 22:10:00 +0800

    2222

---
+++ application/person/controller/Index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac
+ * Date: 2019/7/25
+ * Time: 22:09
+ */
+
+namespace app\person\controller;
+
+
+class Index
+{
+    public function index() {
+        echo 'gdfhgerdt';
+    }
+}
