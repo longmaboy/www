fcea8e702d2e36e438d2fb9b3e3b10b7db30dd41
malongbo <751824643@qq.com>
2019-07-29 12:10:55 +0800

    adfsgd

---
+++ application/malongbo/controller/Login.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac
+ * Date: 2019/7/29
+ * Time: 12:10
+ */
+
+namespace app\malongbo\controller;
+
+
+class Login
+{
+    public function index() {
+        echo 'eagsdfghg';
+    }
+}
