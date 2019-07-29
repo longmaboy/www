91147164e27fd53aba0ba41ce691003714749c63
malongbo <751824643@qq.com>
2019-07-29 13:20:45 +0800

    adfsgd

---
+++ application/malongbo/controller/Savenickname.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac
+ * Date: 2019/7/29
+ * Time: 13:18
+ */
+
+namespace app\malongbo\controller;
+
+use think\Db;
+
+class Savenickname
+{
+    public function index () {
+
+        if (!Token::verification_header_token()) {
+            $data = config()['requestsuccess'];
+            $data['code'] = 301;
+            $data['message'] = 'token error';
+            $data['data'] = '';
+            return json($data);
+        }
+
+        if (isset($_POST['token']) && isset($_POST['verificationcode'])) {
+
+            if (Token::verification_body_token($_POST['token'],$_POST['verificationcode'])) {
+
+                if (!isset($_POST['nickname'])) {
+                    $data = config()['requestsuccess'];
+                    $data['code'] = 203;
+                    $data['message'] = 'iamge param error';
+                    $data['data'] = '';
+                    return json($data);
+                }else{
+
+                    return $this->saveNickName();
+
+                    $data = config()['requestsuccess'];
+                    $data['code'] = 200;
+                    $data['message'] = '11111111';
+                    $data['data'] = '';
+                    return json($data);
+
+                }
+
+            }else{
+
+                $data = config()['requestsuccess'];
+                $data['code'] = 201;
+                $data['message'] = 'param error';
+                $data['data'] = '';
+                return json($data);
+
+            }
+
+        } else{
+
+            $data = config()['requestsuccess'];
+            $data['code'] = 202;
+            $data['message'] = 'param error';
+            $data['data'] = '';
+            return json($data);
+
+        }
+
+    }
+
+
+    protected function saveNickName() {
+
+        $exphone = Db::table('sf_user')->where('id', $_POST['uid'])->find();
+
+        if (isset($exphone) && !empty($exphone)) {
+
+            $nickname = ['nickname' => $_POST['nickname']];
+
+            Db::name('sf_user')
+                ->where('id', $_POST['uid'])
+                ->update($nickname);
+
+            $infoData = [
+                'nickname' => $_POST['nickname'],
+            ];
+
+            $data = config()['requestsuccess'];
+            $data['data'] = $infoData;
+            $data['code'] = 200;
+            $data['message'] = '更新成功';
+            return json($data);
+
+        }else{
+
+            $data = config()['requestsuccess'];
+            $data['data'] = '';
+            $data['code'] = 201;
+            $data['message'] = '未查询到用户';
+            return json($data);
+
+        }
+    }
+}
---
+++ application/malongbo/controller/Uploadphoto.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac
+ * Date: 2019/7/29
+ * Time: 13:19
+ */
+
+namespace app\malongbo\controller;
+
+use app\load\controller\LoadImage;
+
+class Uploadphoto
+{
+    public function index () {
+
+        if (!Token::verification_header_token()) {
+            $data = config()['requestsuccess'];
+            $data['code'] = 301;
+            $data['message'] = 'token error';
+            $data['data'] = '';
+            return json($data);
+        }
+
+        if (isset($_POST['token']) && isset($_POST['verificationcode'])) {
+
+            if (Token::verification_body_token($_POST['token'],$_POST['verificationcode'])) {
+
+                if (!isset($_POST['image'])) {
+                    $data = config()['requestsuccess'];
+                    $data['code'] = 203;
+                    $data['message'] = 'iamge param error';
+                    $data['data'] = '';
+                    return json($data);
+                }else{
+
+                    return LoadImage::myloadimage($_POST['image']);
+                }
+
+            }else{
+
+                $data = config()['requestsuccess'];
+                $data['code'] = 201;
+                $data['message'] = 'param error';
+                $data['data'] = '';
+                return json($data);
+
+            }
+
+        } else{
+
+            $data = config()['requestsuccess'];
+            $data['code'] = 202;
+            $data['message'] = 'param error';
+            $data['data'] = '';
+            return json($data);
+
+        }
+        
+    }
+
+}
