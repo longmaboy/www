ba79ccf00ae2815c56ff9ed3391e6bfcbafa9a8b
malongbo <751824643@qq.com>
2019-07-31 21:49:07 +0800

    feedback

---
+++ application/malongbo/controller/Feedback.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac
+ * Date: 2019/7/31
+ * Time: 21:46
+ */
+
+namespace app\malongbo\controller;
+
+
+class Feedback
+{
+    public function index() {
+
+        if (!Token::verification_header_token()) {
+            $data = config()['requestsuccess'];
+            $data['code'] = 301;
+            $data['message'] = 'token error';
+            $data['data'] = '';
+            return json($data);
+        }
+
+        if (isset($_POST['token']) && isset($_POST['verificationcode'])) {
+
+            if (Token::verification_body_token($_POST['token'],$_POST['verificationcode'])) {
+
+                if (!isset($_POST['contentid']) || !isset($_POST['title']) || !isset($_POST['detail'])) {
+                    $data = config()['requestsuccess'];
+                    $data['code'] = 203;
+                    $data['message'] = 'param error';
+                    $data['data'] = '';
+                    return json($data);
+                }else{
+
+                    return $this->saveFeedbak($_POST['content'],$_POST['uid']);
+                }
+
+            }else{
+
+                $data = config()['requestsuccess'];
+                $data['code'] = 201;
+                $data['message'] = 'param error';
+                $data['data'] = '';
+                return json($data);
+
+            }
+
+        } else{
+
+            $data = config()['requestsuccess'];
+            $data['code'] = 202;
+            $data['message'] = 'param error';
+            $data['data'] = '';
+            return json($data);
+
+        }
+
+    }
+
+    protected function saveFeedbak($content, $uid) {
+
+        $up_data = [
+            'content' => $content,
+            'uid' => $uid,
+        ];
+
+        Db::table('sf_feedback')
+            ->data($up_data)
+            ->insert();
+
+        $data = config()['requestsuccess'];
+        $data['code'] = 200;
+        $data['message'] = '提交成功';
+        $data['data'] = $up_data;
+        return json($data);
+    }
+}
